2ca45b628334fd64dc73e535c40a7e3c15a66609
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-08-25 16:48:53 +0200

    Update g4.php

--- g4.php
+++ g4.php
@@ -3,9 +3,10 @@ require 'vendor/autoload.php';
 //$client = \Symfony\Component\Panther\Client::createChromeClient();
 //$client->setClient(new \GuzzleHttp\Client($config));
 //sleep(mt_rand(200, 1650));
-$client = \Symfony\Component\Panther\Client::createChromeClient(
+$client = \Symfony\Component\Panther\Client::createChromeClient(null, [
   //'--window-size=5000,6000'
-);
+  '--headfull'
+]);
   //$client->setClient(new \GuzzleHttp\Client(['proxy' => '199.217.116.5:5836']));
 sleep(1);
 $crawler = $client->request('GET', 'https://www.youtube.com/results?search_query=php+web+scraping&sp=EgIIBA%253D%253D');
