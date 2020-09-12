b7ca8ea8db98bc3b9110ec4a26132fedfb530541
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-12 12:52:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -212,10 +212,7 @@ if(array_key_exists('button1', $_POST)) {
 } 
 function button1(){
   ignore_user_abort(true);
-  exec("npm install");
-    sleep(300);
-  exec("npm install puppeteer");
-    sleep(300);
+  //exec("npm install");
   exec("node proxy.js &");
 }
 button1();
