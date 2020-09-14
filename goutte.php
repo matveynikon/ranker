33703cfc5a153e8e834f61b412f24582878b0616
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-14 20:21:00 +0200

    Update goutte.php

--- goutte.php
+++ goutte.php
@@ -3,23 +3,12 @@ require 'vendor/autoload.php';
 //$client = \Symfony\Component\Panther\Client::createChromeClient();
 //$client->setClient(new \GuzzleHttp\Client($config));
 //sleep(mt_rand(200, 1650));
-$client = \Symfony\Component\Panther\Client::createChromeClient(null, [
-  '--headfull'
-]);
+$client = \Symfony\Component\Panther\Client::createChromeClient();
 //use Goutte\Client;
 
 //$client = new Client();
   //$client->setClient(new \GuzzleHttp\Client(['proxy' => '199.217.116.5:5836']));
 sleep(1);
-$crawler = $client->request('GET', 'https://www.youtube.com/results?search_query=php+web+scraping+');
+$client->request('GET', 'https://matveynikon.wixsite.com/dailyhero/post/inventions-that-help-the-world');
 sleep(15);
-
-$client->takeScreenshot('shot2.png');   
-$link = $crawler->selectLink('PHP Web Scraping - Simple HTML DOM Parser')->link();
-$crawler = $client->click($link);
-//var_dump($client->getResponse()->getContent());
-//$client->takeScreenshot('shot2.png');
-  //$crawler->clickLink('Python list comprehension[in 2 minutes]');
-sleep(mt_rand(130, 135));
-//$client->takeScreenshot('shot3.png');
 ?>
